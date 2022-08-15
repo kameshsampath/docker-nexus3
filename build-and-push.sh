227795f219975248ebf13c7b59f5eeea40e3e9b3
Kamesh <kamesh.sampath@hotmail.com>
2022-08-15 11:22:37 +0530

    multi arch build with configurator

---
+++ build-and-push.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+set -euxo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+IMAGE_REPO=docker.io/kameshsampath
+BUILDER=buildx-multi-arch
+NEXUS_DOCKER_FILE=$SCRIPT_DIR/Dockerfile
+NEXUS_CONFIG_DOCKER_FILE=$SCRIPT_DIR/Dockerfile
+NEXUS_VERSION=3.41.0-01
+NEXUS_CONFIG_VERSION=$(svu patch)
+
+docker buildx inspect "$BUILDER" || docker buildx create --name="$BUILDER" --driver=docker-container --driver-opt=network=host
+
+docker pull "$IMAGE_REPO/nexus3:$NEXUS_VERSION" && echo "Failure: Tag already exists"
+
+docker buildx build --push --builder="$BUILDER" --platform=linux/amd64,linux/arm64 --build-arg TAG=$NEXUS_VERSION --tag="$IMAGE_REPO/nexus3:$NEXUS_VERSION" -f "$NEXUS_DOCKER_FILE" .
+
+docker buildx build --push --builder="$BUILDER" --platform=linux/amd64,linux/arm64 --build-arg TAG="${NEXUS_CONFIG_VERSION:1}" --tag="$IMAGE_REPO/nexus3-config:${NEXUS_CONFIG_VERSION:1}" -f "$NEXUS_CONFIG_DOCKER_FILE" .
+
+docker tag "$IMAGE_REPO/nexus3:$NEXUS_VERSION" "$IMAGE_REPO/nexus3:latest"
+docker push "$IMAGE_REPO/nexus3:latest"
+docker tag "$IMAGE_REPO/nexus3-config:${NEXUS_CONFIG_VERSION:1}" "$IMAGE_REPO/nexus3-config:latest"
+docker push "$IMAGE_REPO/nexus3-config:latest"
---
+++ config/config.sh
@@ -0,0 +1,119 @@
+#! /usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cp "${SCRIPT_DIR}/nexus.properties" /nexus-data/etc/nexus.properties
+
+set -uo pipefail
+
+NEXUS_SCRIPTS=("anonymous-access" "update-admin-password")
+SCRIPT_FILE=${SCRIPT_FILE:-"${SCRIPT_DIR}/script.json"}
+NEXUS_URL=${NEXUS_URL:-'http://localhost:8081'}
+NEXUS_NS=${NEXUS_NS:-infra}
+NEXUS_ADMIN_PASSWD=${NEXUS_ADMIN_PASSWD:-admin123}
+ANONYMOUS_ACCESS=${ANONYMOUS_ACCESS:-false}
+
+function wait_for_nexus() {
+  res_code=$(curl --silent --fail --output /dev/null -w '%{http_code}' "${NEXUS_URL}")
+
+  until [ "$res_code" -ne 000 ] &&  [ "$res_code" -lt 400 ];
+  do
+    sleep 5
+    res_code=$(curl --silent --fail --output /dev/null -w '%{http_code}' "${NEXUS_URL}")
+  done
+
+  printf "\n Nexus Ready\n"
+}
+
+function check_if_script_exists(){
+  local http_code
+  http_code="$(curl -s -o /dev/null -I -w '%{http_code}' -X GET -u admin:$1 $NEXUS_URL/service/rest/v1/script/$2)"
+  echo "$http_code"
+}
+
+function loadScripts(){
+  local admin_pwd
+  if [ -f  /nexus-data/admin.password ];
+  then
+    admin_pwd=$(cat /nexus-data/admin.password)
+  else
+    admin_pwd="${NEXUS_ADMIN_PASSWD}"
+  fi
+
+  for s in "${NEXUS_SCRIPTS[@]}"
+  do 
+    local script_json_file
+    script_json_file=""${SCRIPT_DIR}/$s.json""
+    local http_code
+    http_code="$(check_if_script_exists $admin_pwd $s)"
+    echo "$http_code"
+
+    if [ "200" != "${http_code}" ];
+    then 
+      echo "Creating $s script"
+      curl -v -X POST --header "Content-Type: application/json" \
+        -u "admin:$admin_pwd" \
+        -d@"$script_json_file" \
+        "$NEXUS_URL/service/rest/v1/script"
+    else
+      echo "Updating $s script"
+      curl -v -X PUT --header "Content-Type: application/json" \
+        -u "admin:$admin_pwd" \
+        -d@"$script_json_file" \
+        "$NEXUS_URL/service/rest/v1/script"
+    fi
+  done
+}
+
+function run_script(){
+ local http_code
+ http_code="$(curl -s -o /dev/null -w '%{http_code}' -X POST -H 'Content-Type: text/plain' -u admin:$1 $NEXUS_URL/service/rest/v1/script/$2/run -d $3)"
+ echo "$http_code"
+}
+
+function update_admin_password(){
+  if [ -f /nexus-data/admin.password ];
+  then
+    local admin_pwd
+    admin_pwd=$(cat /nexus-data/admin.password)
+    printf "\nUpdating the password\n"
+    if [ "200" == "$(run_script $admin_pwd 'update-admin-password' $NEXUS_ADMIN_PASSWD)" ];
+    then
+      printf "\n Password updated successfully \n"
+      rm -f /nexus-data/admin.password
+    else
+      printf "\n Password updated failed \n"
+    fi
+  else
+    printf "\n Skipping Password reset\n"
+  fi
+}
+
+## Enable/Disable Anonymous Access
+function set_anonymous_access() {
+  local admin_pwd
+  if [ -f  /nexus-data/admin.password ];
+  then
+    admin_pwd=$(cat /nexus-data/admin.password)
+  else
+    admin_pwd="${NEXUS_ADMIN_PASSWD}"
+  fi
+
+  if [ "200" == "$(run_script "$admin_pwd" 'anonymous-access' "${ANONYMOUS_ACCESS}")" ];
+    then
+      printf "\n Updated anonymous access \n"
+    else
+      printf "\n Anonymous access update failed \n"
+  fi
+}
+
+wait_for_nexus
+loadScripts
+
+# enable exit on error
+set -e 
+
+update_admin_password
+set_anonymous_access
+
+exit 0
