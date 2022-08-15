8717b86c03759d16db5e4673f58b254c2eba9620
Kamesh <kamesh.sampath@hotmail.com>
2022-08-15 15:51:04 +0530

    tag updates

---
+++ build-and-push-config.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+IMAGE_REPO=docker.io/kameshsampath
+BUILDER=buildx-multi-arch
+NEXUS_CONFIG_DOCKER_FILE=$SCRIPT_DIR/Dockerfile
+NEXUS_VERSION=3.41.0-01
+NEXUS_CONFIG_VERSION=${NEXUS_VERSION}-config
+
+docker buildx inspect "$BUILDER" || docker buildx create --name="$BUILDER" --driver=docker-container --driver-opt=network=host
+
+docker buildx build \
+  --push \
+  --builder="$BUILDER" \
+  --platform=linux/amd64,linux/arm64 \
+  --build-arg TAG="${NEXUS_CONFIG_VERSION}" \
+  --tag="$IMAGE_REPO/nexus3-config:${NEXUS_CONFIG_VERSION}" \
+  --tag="$IMAGE_REPO/nexus3-config:latest" \
+  -f "$NEXUS_CONFIG_DOCKER_FILE" .
--- build-and-push-nexus.sh
+++ build-and-push-nexus.sh
@@ -7,9 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 IMAGE_REPO=docker.io/kameshsampath
 BUILDER=buildx-multi-arch
 NEXUS_DOCKER_FILE=$SCRIPT_DIR/Dockerfile
-NEXUS_CONFIG_DOCKER_FILE=$SCRIPT_DIR/Dockerfile
 NEXUS_VERSION=3.41.0-01
-NEXUS_CONFIG_VERSION=$(svu patch)
 
 docker buildx inspect "$BUILDER" || docker buildx create --name="$BUILDER" --driver=docker-container --driver-opt=network=host
 
@@ -20,18 +18,5 @@ docker buildx build --push \
   --platform=linux/amd64,linux/arm64 \
   --build-arg TAG=$NEXUS_VERSION \
   --tag="$IMAGE_REPO/nexus3:$NEXUS_VERSION" \
+  --tag="$IMAGE_REPO/nexus3:latest" \
   -f "$NEXUS_DOCKER_FILE" .
-
-docker buildx build \
-  --push \
-  --builder="$BUILDER" \
-  --platform=linux/amd64,linux/arm64 \
-  --build-arg TAG="${NEXUS_CONFIG_VERSION:1}" \
-  --tag="$IMAGE_REPO/nexus3-config:${NEXUS_CONFIG_VERSION:1}" \
-  -f "$NEXUS_CONFIG_DOCKER_FILE" .
-
-docker tag "$IMAGE_REPO/nexus3:$NEXUS_VERSION" "$IMAGE_REPO/nexus3:latest"
-docker push "$IMAGE_REPO/nexus3:latest"
-
-docker tag "$IMAGE_REPO/nexus3-config:${NEXUS_CONFIG_VERSION:1}" "$IMAGE_REPO/nexus3-config:latest"
-docker push "$IMAGE_REPO/nexus3-config:latest"
